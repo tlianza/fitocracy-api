cd8e61109269bd6c6c82e74c02a59ad742739939
Tom Lianza <tom@lianza.org>
2016-01-10 17:40:51 -0800

    Added UI, sessions, and ability to generate a CSV of a given activity.

--- login_api.rb
+++ login_api.rb
@@ -2,17 +2,38 @@ require 'sinatra'
 require 'mechanize'
 require 'json'
 require 'pry'
+require 'csv'
 require_relative 'models/user'
 require_relative 'page_models/login'
 require_relative 'lib/fitocracy/activity'
+
+set :sessions, true
+set :session_secret, 'change_me'
 
 get '/' do
   erb :index
 end
 
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  session[:username] = request.POST['username']
+  session[:password] = request.POST['password']
+  redirect to("/user/activities")
+end
+
+get '/logout' do
+  session[:username] = nil
+  session[:password] = nil
+  redirect to('/login')
+end
+
+
 before '/user/*' do
   @agent = Mechanize.new
-  @user  = User.new({ agent: @agent})
+  @user  = User.new({ agent: @agent, username:session[:username], password:session[:password]})
 
   halt(401, @user.error) if @user.error
 
@@ -23,19 +44,18 @@ before '/user/*' do
   halt(401, login_json['error']) unless login_json['success']
 
   @user.x_fitocracy_user  = login_response["X-Fitocracy-User"]
+  #logger.info("x_fitocracy_user: #{@user.x_fitocracy_user}")
 end
 
-get '/user/:username/activities' do
-  activity = ::Fitocracy::Activity.new(user:  @user,
-                                       agent: @agent)
-
+get '/user/activities' do
+  activity = ::Fitocracy::Activity.new(user:  @user, agent: @agent)
   all_activites_data = activity.get_all_activities_for_user
 
   content_type :json
   JSON.pretty_generate(JSON.parse(all_activites_data.body))
 end
 
-get '/user/:username/activity/:activity_name' do
+get '/user/activity/:activity_name' do
   activity = ::Fitocracy::Activity.new(user:          @user,
                                        agent:         @agent,
                                        activity_name: params[:activity_name])
@@ -44,6 +64,22 @@ get '/user/:username/activity/:activity_name' do
 
   content_type :json
   JSON.pretty_generate(JSON.parse(activity_data.body))
+end
+
+get '/user/activity/:activity_name/export' do
+  activity = ::Fitocracy::Activity.new(user: @user, agent: @agent, activity_name: params[:activity_name])
+  data = JSON.parse(activity.activity_log.body)
+
+  csv_string = CSV.generate do |csv|
+    data.each do |child|
+      child['actions'].each do |action|
+        csv << [params[:activity_name], child['date'], action['effort1'], action['effort1_unit']['abbr'], action['effort0'], action['effort0_unit']['abbr']]
+      end
+    end
+  end
+
+  content_type 'text/csv'
+  csv_string
 end
 
 get '/charts' do
